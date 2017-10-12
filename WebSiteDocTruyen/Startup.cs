4c9e19df9b65d2850e79bcb2f0f43567e7fa7609
User <User@DESKTOP-QR164ON>
2017-10-12 22:20:12 +0700

    Add project files.

---
+++ WebSiteDocTruyen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteDocTruyen.Startup))]
+namespace WebSiteDocTruyen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
